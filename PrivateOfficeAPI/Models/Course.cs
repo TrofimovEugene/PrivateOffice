5983a5a72380c3f958c6cbf21a5117126005eb0b
Trofimov Eugene <evgentrofi@gmail.com>
2020-01-26 02:11:17 +0800

    Создал запросы к серверу и обращение через них к БД.

---
+++ PrivateOfficeAPI/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+
+namespace PrivateOfficeAPI.Models
+{
+    [JsonObject]
+    public class Course
+    {
+        [JsonProperty("idCourse")]
+        public int IdCourse { get; set; }
+        [JsonProperty("nameCourse")]
+        public string NameCourse { get; set; }
+    }
+}
