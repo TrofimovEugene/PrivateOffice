1ee89647107b34829b2c74baf60b6fb34b43e5b7
Julia <negodyaeva.yulya@mail.ru>
2020-01-27 22:00:05 +0800

    Добавлена миграция базы данных https://github.com/TrofimovEugene/PrivateOffice/issues/2

---
+++ PrivateOfficeDataBaseAPI/Migrations/20200127134641_DataBaseTeachers.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrivateOfficeDataBaseAPI.Migrations
+{
+    public partial class DataBaseTeachers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Teacher",
+                columns: table => new
+                {
+                    IdTeacher = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Login = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    SecondName = table.Column<string>(nullable: true),
+                    Patronymic = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teacher", x => x.IdTeacher);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Course",
+                columns: table => new
+                {
+                    IdCourse = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NameCourse = table.Column<string>(nullable: true),
+                    IdTeacher = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Course", x => x.IdCourse);
+                    table.ForeignKey(
+                        name: "FK_Course_Teacher_IdTeacher",
+                        column: x => x.IdTeacher,
+                        principalTable: "Teacher",
+                        principalColumn: "IdTeacher",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Classes",
+                columns: table => new
+                {
+                    IdClasses = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NameClasses = table.Column<string>(nullable: true),
+                    StartTime = table.Column<TimeSpan>(nullable: false),
+                    EndTime = table.Column<TimeSpan>(nullable: false),
+                    CountTime = table.Column<int>(nullable: false),
+                    IdCourse = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classes", x => x.IdClasses);
+                    table.ForeignKey(
+                        name: "FK_Classes_Course_IdCourse",
+                        column: x => x.IdCourse,
+                        principalTable: "Course",
+                        principalColumn: "IdCourse",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Classes_IdCourse",
+                table: "Classes",
+                column: "IdCourse");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Course_IdTeacher",
+                table: "Course",
+                column: "IdTeacher");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Classes");
+
+            migrationBuilder.DropTable(
+                name: "Course");
+
+            migrationBuilder.DropTable(
+                name: "Teacher");
+        }
+    }
+}
