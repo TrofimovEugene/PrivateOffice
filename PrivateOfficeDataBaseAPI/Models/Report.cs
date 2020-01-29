f8405b111e152dd3cfa2be8e50710211b49a9308
Julia <negodyaeva.yulya@mail.ru>
2020-01-29 14:02:09 +0800

    Добавлена таблица "Доклад" https://github.com/TrofimovEugene/PrivateOffice/issues/9

---
+++ PrivateOfficeDataBaseAPI/Models/Report.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrivateOfficeDataBaseAPI.Models
+{
+    public class Report
+    {
+
+        [Key]
+        public int IdReport { get; set; }
+        [ForeignKey("Student")]
+        public int IdStudent { get; set; }
+        [ForeignKey("Classes")]
+        public int IdClasses { get; set; }
+        public string NameReport { get; set; }
+    }
+}
