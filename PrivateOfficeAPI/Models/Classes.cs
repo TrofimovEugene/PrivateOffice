d15f4cc951e0a1f0ce04ca1768a773e3e6bc0498
Trofimov Eugene <evgentrofi@gmail.com>
2020-01-31 23:24:14 +0800

    Добавил отображение занятий.

---
+++ PrivateOfficeAPI/Models/Classes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace PrivateOfficeAPI.Models
+{
+	[JsonObject]
+    public class Classes
+    {
+		[JsonProperty("idClasses")]
+		public int IdClasses { get; set; }
+		[JsonProperty("idTypeClasses")]
+		public int IdTypeClasses { get; set; }
+		[JsonProperty("idCourse")]
+		public int IdCourse { get; set; }
+		[JsonProperty("nameClasses")]
+		public string NameClasses { get; set; }
+		[JsonProperty("startTime")]
+		public DateTime StartTime { get; set; }
+		[JsonProperty("endTime")]
+		public DateTime EndTime { get; set; }
+		[JsonProperty("daysWeek")]
+		public string DaysWeek { get; set; }
+		[JsonProperty("countClasses")]
+		public string CountClasses { get; set; }
+		[JsonProperty("countTime")]
+		public int CountTime { get; set; }
+	}
+}
--- PrivateOfficeWebApp/Models/Course.cs
+++ PrivateOfficeWebApp/Models/Course.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace PrivateOfficeWebApp.Models
 {
@@ -6,10 +8,18 @@ namespace PrivateOfficeWebApp.Models
     public class Course
     {
         [JsonProperty("idCourse")]
-        public int? IdCourse { get; set; }
+        public int IdCourse { get; set; }
         [JsonProperty("nameCourse")]
         public string NameCourse { get; set; }
         [JsonProperty("idTeacher")]
 		public int IdTeacher { get; set; }
+        [JsonProperty("startDate")]
+        public DateTime StartDate { get; set; }
+        [JsonProperty("endDate")]
+        public DateTime EndDate { get; set; }
+        [JsonProperty("nameUniversity")]
+        public string NameUniversity { get; set; }
+        [JsonProperty("classes")]
+        public virtual IList<Classes> Classes { get; set; }
     }
 }
