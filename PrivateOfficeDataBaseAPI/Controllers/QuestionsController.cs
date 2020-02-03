c83bba1e9727bab3e543790b2888d875ed147e9e
Julia <negodyaeva.yulya@mail.ru>
2020-02-03 14:35:49 +0800

    Добавлены контроллеры

---
+++ PrivateOfficeDataBaseAPI/Controllers/QuestionsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PrivateOfficeDataBaseAPI.Data;
+using PrivateOfficeDataBaseAPI.Models;
+
+namespace PrivateOfficeDataBaseAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly PrivateOfficeDataBaseAPIContext _context;
+
+        public QuestionsController(PrivateOfficeDataBaseAPIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Questions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Questions>>> GetQuestions()
+        {
+            return await _context.Questions.ToListAsync();
+        }
+
+        // GET: api/Questions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Questions>> GetQuestions(int id)
+        {
+            var questions = await _context.Questions.FindAsync(id);
+
+            if (questions == null)
+            {
+                return NotFound();
+            }
+
+            return questions;
+        }
+
+        // PUT: api/Questions/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutQuestions(int id, Questions questions)
+        {
+            if (id != questions.IdQuestions)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(questions).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!QuestionsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Questions
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Questions>> PostQuestions(Questions questions)
+        {
+            _context.Questions.Add(questions);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetQuestions", new { id = questions.IdQuestions }, questions);
+        }
+
+        // DELETE: api/Questions/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Questions>> DeleteQuestions(int id)
+        {
+            var questions = await _context.Questions.FindAsync(id);
+            if (questions == null)
+            {
+                return NotFound();
+            }
+
+            _context.Questions.Remove(questions);
+            await _context.SaveChangesAsync();
+
+            return questions;
+        }
+
+        private bool QuestionsExists(int id)
+        {
+            return _context.Questions.Any(e => e.IdQuestions == id);
+        }
+    }
+}
---
+++ PrivateOfficeDataBaseAPI/Controllers/TasksController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PrivateOfficeDataBaseAPI.Data;
+using PrivateOfficeDataBaseAPI.Models;
+using Task = PrivateOfficeDataBaseAPI.Models.Task;
+
+namespace PrivateOfficeDataBaseAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly PrivateOfficeDataBaseAPIContext _context;
+
+        public TasksController(PrivateOfficeDataBaseAPIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tasks
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Models.Task>>> GetTask()
+        {
+            return await _context.Task.ToListAsync();
+        }
+
+        // GET: api/Tasks/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Models.Task>> GetTask(int id)
+        {
+            var task = await _context.Task.FindAsync(id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return task;
+        }
+
+        // PUT: api/Tasks/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTask(int id, Models.Task task)
+        {
+            if (id != task.IdQuestions)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(task).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaskExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tasks
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Task>> PostTask(Task task)
+        {
+            _context.Task.Add(task);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTask", new { id = task.IdQuestions }, task);
+        }
+
+        // DELETE: api/Tasks/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Task>> DeleteTask(int id)
+        {
+            var task = await _context.Task.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            _context.Task.Remove(task);
+            await _context.SaveChangesAsync();
+
+            return task;
+        }
+
+        private bool TaskExists(int id)
+        {
+            return _context.Task.Any(e => e.IdQuestions == id);
+        }
+    }
+}
