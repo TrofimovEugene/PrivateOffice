92fd36daf031adba74c7379619440c5236cfc09e
Trofimov Eugene <evgentrofi@gmail.com>
2020-02-06 23:49:41 +0800

    Вывод курсов для определённого преподавателя.

--- PrivateOfficeWebApp/AppSettings.cs
+++ PrivateOfficeWebApp/AppSettings.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace PrivateOfficeWebApp
+﻿namespace PrivateOfficeWebApp
 {
 	public static class AppSettings
 	{
