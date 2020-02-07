27860abc3c1df785bfb4adad4591074cb1bbc626
Julia <negodyaeva.yulya@mail.ru>
2020-02-07 18:28:48 +0800

    Редактирование и удаление студента

--- PrivateOfficeWebApp/Pages/StudentsTable/StudentsTable.cshtml.cs
+++ PrivateOfficeWebApp/Pages/StudentsTable/StudentsTable.cshtml.cs
@@ -13,61 +13,66 @@ namespace PrivateOfficeWebApp
 {
     public class StudentsTableModel : PageModel
     {
-	    private readonly HttpClient _httpClient;
+		private readonly HttpClient _httpClient;
 
-	    public StudentsTableModel()
-	    {
-		    var clientHandler = new HttpClientHandler
-		    {
-			    ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true
-		    };
-		    _httpClient = new HttpClient(clientHandler);
+		public StudentsTableModel()
+		{
+			var clientHandler = new HttpClientHandler
+			{
+				ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true
+			};
+			_httpClient = new HttpClient(clientHandler);
 		}
 		[BindProperty]
 		public List<Student> Students { get; set; }
-        public async Task<IActionResult> OnGet(int id)
-        {
-	        HttpResponseMessage response = await _httpClient.GetAsync("https://localhost:44316/api/Students");
-	        var jsonResponse = await response.Content.ReadAsStringAsync();
-	        Students = JsonConvert.DeserializeObject<List<Student>>(jsonResponse);
+		public async Task<IActionResult> OnGet(int id)
+		{
+			HttpResponseMessage response = await _httpClient.GetAsync("https://localhost:44316/api/Students");
+			var jsonResponse = await response.Content.ReadAsStringAsync();
+			Students = JsonConvert.DeserializeObject<List<Student>>(jsonResponse);
 
-	        foreach (var student in Students)
-	        {
+			foreach (var student in Students)
+			{
 				response = await _httpClient.GetAsync("https://localhost:44316/api/Groups/" + student.IdGroup);
 				jsonResponse = await response.Content.ReadAsStringAsync();
 				var group = JsonConvert.DeserializeObject<Group>(jsonResponse);
 				student.Group = group;
-	        }
-	        
+			}
+
 			response = await _httpClient.GetAsync("https://localhost:44316/api/Groups/");
-	        jsonResponse = await response.Content.ReadAsStringAsync();
-	        Groups = JsonConvert.DeserializeObject<List<Group>>(jsonResponse);
+			jsonResponse = await response.Content.ReadAsStringAsync();
+			Groups = JsonConvert.DeserializeObject<List<Group>>(jsonResponse);
 
 			return Page();
-        }
+		}
 		[BindProperty]
 		public ResponseStudent Student { get; set; }
 		[BindProperty]
 		public List<Group> Groups { get; set; }
-        public async Task<IActionResult> OnPost(int idgroup)
-        {
-	        Student.IdGroup = idgroup;
-	        var jsonRequest = JsonConvert.SerializeObject(Student);
+		public async Task<IActionResult> OnPostCreateStudent(int idgroup)
+		{
+			Student.IdGroup = idgroup;
+			var jsonRequest = JsonConvert.SerializeObject(Student);
 			HttpContent httpContent = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
 			await _httpClient.PostAsync("https://localhost:44316/api/Students", httpContent);
 
 			return RedirectToPage("./StudentsTable");
-        }
+		}
 
-        [JsonObject]
-        public class ResponseStudent
-        {
-	        [JsonProperty("idGroup")] 
-	        public int IdGroup { get; set; }
-	        [JsonProperty("firstName")] 
-	        public string FirstName { get; set; }
-	        [JsonProperty("secondName")] 
-	        public string SecondName { get; set; }
-        }
-    }
+		[JsonObject]
+		public class ResponseStudent
+		{
+			[JsonProperty("idGroup")]
+			public int IdGroup { get; set; }
+			[JsonProperty("firstName")]
+			public string FirstName { get; set; }
+			[JsonProperty("secondName")]
+			public string SecondName { get; set; }
+		}
+		public async Task<IActionResult> OnPostDelete(int id)
+		{
+			await _httpClient.DeleteAsync("https://localhost:44316/api/Students/" + id);
+			return RedirectToPage("./StudentsTable");
+		}
+	}
 }
