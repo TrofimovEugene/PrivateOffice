6d4fc64fb58d932fcc0189eee5441ffffc9e1c79
NoMaD38 <nomad38note@gmail.com>
2020-02-12 20:00:03 +0800

    Немного поправил LoadCheck

--- Test/CheckLogin/LoadCheck.js
+++ Test/CheckLogin/LoadCheck.js
@@ -3,61 +3,72 @@ const puppeteer = require('puppeteer');
 async function getCheckScreen() {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
+
+  let user = {
+    firstName:'Test',
+    secondName:'Test',
+    patronymic:'Test',
+    login:'test123',
+    password:'test123' 
+  }
   
   try {
 
   await page.goto('https://localhost:44326/');
   await page.waitFor(1000);
   await page.setViewport({width: 1000, height: 700})
-  await page.screenshot({path: 'Главный экран.png'});
+  await page.screenshot({path: './result/Главный экран.png'});
 
   await page.click('#nav-teacher > form > div.d-flex.justify-content-between > input.btn.btn-primary')
-  await page.screenshot({path: 'Проверка входа учителя.png'});
+  await page.screenshot({path: './result/Проверка входа учителя.png'});
 
   await page.click('#nav-student-tab')
-  await page.screenshot({path: 'Окно входа студента.png'});
+  await page.waitFor(1000);
+  await page.click('#nav-student > form > div.d-flex.justify-content-end > button.btn.btn-primary')
+  await page.screenshot({path: './result/Проверка входа студента.png'});
 
   await page.click('#nav-teacher-tab')
   await page.waitFor(1000);
   await page.click('#nav-teacher > form > div.d-flex.justify-content-between > input.btn.btn-secondary')
   await page.waitFor(1000);
-  await page.screenshot({path: 'Окно регистрации.png'});
+  await page.screenshot({path: './result/Окно регистрации.png'});
 
   await page.focus('#Teacher_FirstName')
-  page.keyboard.type('Test')
+  page.keyboard.type(user.firstName)
   await page.waitFor(1000);
   await page.focus('#Teacher_SecondName')
-  page.keyboard.type('Test')
+  page.keyboard.type(user.secondName)
   await page.waitFor(1000);
   await page.focus('#Teacher_Patronymic')
-  page.keyboard.type('Test')
+  page.keyboard.type(user.patronymic)
   await page.waitFor(1000);
   await page.focus('#Teacher_Login')
-  page.keyboard.type('test123')
+  page.keyboard.type(user.login)
   await page.waitFor(1000);
   await page.focus('#Teacher_Password')
-  page.keyboard.type('test123')
-  await page.screenshot({path: 'Заполненное окно регистрации.png'});
+  page.keyboard.type(user.password)
+  await page.screenshot({path: './result/Заполненное окно регистрации.png'});
   await page.waitFor(1000);
   await page.click('#addTeacher > div > div > div.modal-footer.d-flex.justify-content-between > input.btn.btn-primary')
   await page.waitFor(1000);
 
   await page.focus('#inputTeacherLogin')
-  page.keyboard.type('test123')
+  page.keyboard.type(user.login)
   await page.waitFor(1000);
   await page.focus('#inputTeacherPassword')
-  page.keyboard.type('test123')
-  await page.screenshot({path: 'Заполненное окно входа.png'});
+  page.keyboard.type(user.password)
+  await page.screenshot({path: './result/Заполненное окно входа.png'});
   await page.waitFor(1000);
   await page.click('#nav-teacher > form > div.d-flex.justify-content-between > input.btn.btn-primary')
+
   await page.waitFor(1000);
-  await page.screenshot({path: 'Личный кабинет учителя.png'});
+  await page.screenshot({path: './result/Личный кабинет учителя.png'});
 
   await browser.close();
   console.log('Тест успешно пройден')
 
-  } catch (error) {
-    console.log('Error:', error);
+  } catch (error) { 
+      console.log(error)
   }
   
 }
