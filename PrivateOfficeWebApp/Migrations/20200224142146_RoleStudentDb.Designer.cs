7d1c50a1e557ed322e825c24d9f91fc84e6efcc9
Julia <negodyaeva.yulya@mail.ru>
2020-02-24 22:33:58 +0800

    Добавлено поле "Роль" в таблице "Студентов", обновлена миграция

--- PrivateOfficeWebApp/Migrations/20200224142146_RoleStudentDb.Designer.cs
+++ PrivateOfficeWebApp/Migrations/20200224142146_RoleStudentDb.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PrivateOfficeWebApp.Data;
 
 namespace PrivateOfficeWebApp.Migrations
 {
     [DbContext(typeof(PrivateOfficeWebAppContext))]
-    partial class PrivateOfficeWebAppContextModelSnapshot : ModelSnapshot
+    [Migration("20200224142146_RoleStudentDb")]
+    partial class RoleStudentDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
