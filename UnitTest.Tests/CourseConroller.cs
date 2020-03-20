9b08c59ee3f9995d3232dca49d756d7c67172e5b
Julia <negodyaeva.yulya@mail.ru>
2020-03-20 22:24:27 +0800

    добавлен unit test на проверку количества администраторов

---
+++ UnitTest.Tests/CourseConroller.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrivateOfficeWebApp.Controllers;
+using PrivateOfficeWebApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace UnitTest.Tests
+{
+    
+    public class CourseConroller
+    {
+        private PrivateOfficeWebAppContext _privateOfficeConext ;
+
+        [Fact]
+        public void GetTeasher()
+        {
+            var options = new DbContextOptionsBuilder<PrivateOfficeWebAppContext>()
+                .EnableSensitiveDataLogging()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            using (_privateOfficeConext = new PrivateOfficeWebAppContext(options.Options))
+            {
+                var controller = new TeachersController(_privateOfficeConext);
+                var result = controller.GetTeacher(1);
+                Assert.Equal(0, _privateOfficeConext.Teacher.Count(p => p.Role == "admin"));
+            }
+        }
+    }
+}
